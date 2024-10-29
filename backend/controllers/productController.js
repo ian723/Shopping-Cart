330713a771c2116d0078744b0f525d5235815156
Ian Ndegwa <ndegwaian241@gmail.com>
2024-10-29 16:43:31 +0300

    message

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,6 @@ exports.getProducts = (req, res) => {
     const products = [
         { id: 1, name: 'Product ', price: 100 },
         { id: 2, name: 'Product 2', price: 200 },
-        // Add more products as needed
     ];
     res.json(products);
 };
